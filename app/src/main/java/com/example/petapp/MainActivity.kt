fce59a8806253facadb442769d76faa315a94ecf
Esraa Ahmed <147179829+2004esra@users.noreply.github.com>
2025-12-12 19:30:13 +0200

    modify mainActivity and comment firebase

--- app/src/main/java/com/example/petapp/MainActivity.kt
+++ app/src/main/java/com/example/petapp/MainActivity.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.petapp.ui.navigation.AppNavGraph
 import com.example.petapp.ui.theme.PetAppTheme
+//import com.google.firebase.Firebase
+//
+//import com.google.firebase.ktx.Firebase
+//import com.google.firebase.auth.ktx.auth
+//
+//val auth = Firebase.auth
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +20,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PetAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+                AppNavGraph(navController = navController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PetAppTheme {
-        Greeting("Android")
     }
 }
